9b07e53b86f3f12dee6b329087d295fbb0a88317
PandaaCode95 <alvaro.amairal@gmail.com>
2022-07-30 12:00:56 +0200

    Soluciones modulo1

---
+++ Modulo1/Ejercicio1/app/Program.cs
@@ -0,0 +1,9 @@
+﻿//Pedimos por consola la var;
+string nombre = Console.ReadLine();
+//La imprimimos:
+Console.WriteLine("Hola, como estas:" + nombre +"?");
+//Pedimos todos los datos de datetime
+DateTime now = DateTime.Now;
+//Guardamos lo que nos interesa y lo imprimimos.
+var hora = now.TimeOfDay;
+Console.WriteLine("La hora es : "+hora);
