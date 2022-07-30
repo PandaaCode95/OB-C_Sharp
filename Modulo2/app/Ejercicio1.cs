9bf06a37f54190ed71828c23be7bf1772c489485
PandaaCode95 <alvaro.amairal@gmail.com>
2022-07-30 12:57:29 +0200

    Ejercicios Modulo2

---
+++ Modulo2/app/Ejercicio1.cs
@@ -0,0 +1,46 @@
+﻿//Ejercicio 1
+
+// Console.WriteLine("Indica tu nombre:");
+// string name = Console.ReadLine();
+
+// Console.WriteLine("Indica tu apellido:");
+// string sname =Console.ReadLine();
+
+// Console.WriteLine("Indica tu edad:");
+// int age =Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Indica si sabes programar:");
+// bool code =Convert.ToBoolean(Console.ReadLine());
+
+// Console.WriteLine("El alumno "+ name + " " +sname+ "con edad de: "+ age + "¿Es cierto que sabe programar?" + code);
+
+
+//Ejercicio 2
+
+// //Coche
+
+// int puertas;
+// int ruedas;
+// string marca;
+// bool ITV;
+
+// // //Mesa
+
+// float peso;
+// float largo;
+// string material;
+// string color;
+
+//Ejercicio 3
+
+int num = 22;
+int num2 = 18;
+int num3 = 25;
+
+char letra = 'b';
+char b = 'b';
+char a ='a';
+Console.WriteLine("Es mayor a 18?" + (num > num2));
+Console.WriteLine("Es la var 'letra' igual a 'a'?" + (letra == 'a'));
+Console.WriteLine("Es mayor a 18?" + ((num > num2) && (num < num3)));
+Console.WriteLine("Es la var 'letra' igual a 'a' o b?" + ((letra == a) || (letra == b)));
